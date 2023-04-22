b0880ba5668523ac11e2f51b2f4186edf8a3238b
lucianreale <lucianreale@gmail.com>
2023-04-22 19:28:38 -0300

    creacion de componentes y servicio

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- Angular/src/app/components/about/about.component.ts
+++ Angular/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PortfolioService } from "../../services/portfolio.service"
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  constructor (private datosPortfolio:PortfolioService){
 
+  }
+
+  ngOnInit(){
+    this.datosPortfolio.obtenerDatos()
+  }
 }
